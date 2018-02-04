66fe40831f5efe877fb500236372ef80fcbff187
锡炎 <xiyan.xxm@alibaba-inc.com>
2018-02-04 18:32:28 +0800

    feature. add page 3,4,5

--- webpack/deploy.js
+++ webpack/deploy.js
@@ -2,6 +2,6 @@ var path = require('path');
 var fs = require('fs');
 var index = require('./routers.deploy.json').index;
 
-var html = fs.readFileSync(path.join(__dirname, '..', '/root/dist/', index));
+var html = fs.readFileSync(path.join(__dirname, '..', '/dist/', index));
 
-fs.writeFileSync(path.join(__dirname, '..', '/root/dist/', 'index.html'), html);
+fs.writeFileSync(path.join(__dirname, '..', '/dist/', 'index.html'), html);
